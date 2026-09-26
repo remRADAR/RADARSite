0cd76cc163d0848ec9df875ef733213a5d04c4b2
remRADAR <3.10276257e+08+remRADAR@users.noreply.github.com>
2026-09-26 16:49:19 +0000

    Include sharp native runtime in server trace

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    "/*": [
+      "./node_modules/sharp/**/*",
+      "./node_modules/@img/sharp-linux-x64/**/*",
+      "./node_modules/@img/sharp-libvips-linux-x64/**/*",
+    ],
+  },
   images: {
     remotePatterns: [
       {
